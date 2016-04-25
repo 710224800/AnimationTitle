66c9ff981de5e9cb03dc5afb48062c8581e7f0db
luyh <luyh@cnepay.com>
2016-04-25 17:27:21 +0800

    init

---
+++ AnimationTitle/atitlelayout/src/main/java/com/luyh/atitlelayout/AnimationTitleLayout.java
@@ -0,0 +1,22 @@
+package com.luyh.atitlelayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by luyh on 2016/3/21.
+ */
+public class AnimationTitleLayout extends LinearLayout {
+    public AnimationTitleLayout(Context context) {
+        super(context);
+    }
+
+    public AnimationTitleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AnimationTitleLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
